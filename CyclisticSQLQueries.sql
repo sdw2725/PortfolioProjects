e7798ab92232621b89192ce736d3dfadf7bcf6ed
sdw2725 <111386390+sdw2725@users.noreply.github.com>
2022-12-07 13:14:31 -0600

    Add files via upload

--- CyclisticSQLQueries.sql
+++ CyclisticSQLQueries.sql
@@ -44,19 +44,22 @@ FROM Aug2022
 SELECT *
 FROM ride_data
 
---Add ride length column
+--Add ride length column, convert to datetime then int in order to calcuate averages
 ALTER TABLE ride_data
 Add ride_length time
 
 UPDATE ride_data
 SET ride_length = (ended_at - started_at)
 
+UPDATE ride_data
+SET ride_length = CONVERT(datetime,ride_length)
+
 --add month column
 ALTER TABLE ride_data
 Add month char 
 
 UPDATE ride_data
-SET month = MONTH(started_at)
+SET month = DATENAME(MONTH, started_at)
 
 --add weekday column
 ALTER TABLE ride_data
@@ -65,13 +68,106 @@ ADD weekday nvarchar
 UPDATE ride_data
 SET weekday = DATENAME(WEEKDAY, started_at)
 
---Create View for visualization
-Create View RideDurationByWeekday as
-SELECT member_casual, ride_length, month, weekday
+--View 0:00 minute rides
+Select ride_length
 FROM ride_data
+WHERE ride_length = '00:00:00.0000000'
+
+--Delete 0:00 minute rides
+DELETE 
+FROM ride_data
+WHERE ride_length = '00:00:00.0000000'
+
+--Find totals for members and casual riders
+SELECT Count(ride_id) Member_Rides, member_casual
+FROM ride_data
+WHERE member_casual = 'member'
+GROUP BY member_casual
+SELECT Count(ride_id) Casual_Rides, member_casual
+FROM ride_data
+WHERE member_casual = 'casual'
+GROUP BY member_casual
+
+--Find totals by season
+SELECT Count(ride_id) Summer, member_casual
+FROM ride_data
+WHERE member_casual = 'casual' AND month = 'June'
+OR member_casual = 'casual' AND month = 'July'
+OR member_casual = 'casual' AND month = 'July'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Summer, member_casual
+FROM ride_data
+WHERE member_casual = 'member' AND month = 'June'
+OR member_casual = 'member' AND month = 'July'
+OR member_casual = 'member' AND month = 'July'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Fall, member_casual
+FROM ride_data
+WHERE member_casual = 'casual' AND month = 'August'
+OR member_casual = 'casual' AND month = 'September'
+OR member_casual = 'casual' AND month = 'October'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Fall, member_casual
+FROM ride_data
+WHERE member_casual = 'member' AND month = 'August'
+OR member_casual = 'member' AND month = 'September'
+OR member_casual = 'member' AND month = 'October'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Winter, member_casual
+FROM ride_data
+WHERE member_casual = 'casual' AND month = 'December'
+OR member_casual = 'casual' AND month = 'January'
+OR member_casual = 'casual' AND month = 'February'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Winter, member_casual
+FROM ride_data
+WHERE member_casual = 'member' AND month = 'December'
+OR member_casual = 'member' AND month = 'January'
+OR member_casual = 'member' AND month = 'February'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Spring, member_casual
+FROM ride_data
+WHERE member_casual = 'casual' AND month = 'March'
+OR member_casual = 'casual' AND month = 'April'
+OR member_casual = 'casual' AND month = 'May'
+GROUP BY member_casual
+
+SELECT Count(ride_id) Spring, member_casual
+FROM ride_data
+WHERE member_casual = 'member' AND month = 'March'
+OR member_casual = 'member' AND month = 'April'
+OR member_casual = 'member' AND month = 'May'
+GROUP BY member_casual
+
+select top 5 *
+from ride_data
+
+--Find most popular stations for casual riders
+SELECT TOP 5 count(start_station_name) NumberofRides, start_station_name
+FROM ride_data
+WHERE member_casual = 'casual'
+GROUP BY start_station_name
+ORDER BY NumberofRides desc
+
+
+--Create View for visualization #1
+Create View RideLengthWeekday as
+SELECT ride_id, member_casual, ride_length, month, weekday
+FROM ride_data
+ORDER BY member_casual
 
 SELECT *
-FROM RideDurationByWeekday
+FROM RideLengthWeekday
+
+
+
+
 
 
 
